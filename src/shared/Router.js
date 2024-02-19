0286b391bacbc8cb1e6c87614edfe26c1bea719e
leejieun2023 <lge9380@naver.com>
2024-02-19 10:38:10 +0900

    Initial commit

---
+++ src/shared/Router.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from 'pages/Home';
+import Detail from 'pages/Detail';
+
+function Router() {
+
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+};
+
+export default Router;
